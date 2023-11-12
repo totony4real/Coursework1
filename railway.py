e2b1c6c9cc13e0cec8795bfe1e89534bc4d04098
“Weibin <japi9008@gmail.com>
2023-11-12 19:34:23 +0000

    Initial commit

---
+++ railway.py
@@ -0,0 +1,99 @@
+import matplotlib.pyplot as plt
+
+
+def fare_price(distance, different_regions, hubs_in_dest_region):
+    raise NotImplementedError
+
+
+class Station:
+    def distance_to(self):
+        raise NotImplementedError
+
+
+class RailNetwork:
+    def regions(self):
+        raise NotImplementedError
+
+    def n_stations(self):
+        raise NotImplementedError
+
+    def hub_stations(self, region):
+        raise NotImplementedError
+
+    def closest_hub(self, s):
+        raise NotImplementedError
+
+    def journey_planner(self, start, dest):
+        raise NotImplementedError
+
+    def journey_fare(self, start, dest, summary):
+        raise NotImplementedError
+
+    def plot_fares_to(self, crs_code, save, ADDITIONAL_ARGUMENTS):
+        raise NotImplementedError
+
+    def plot_network(self, marker_size: int = 5) -> None:
+        """
+        A function to plot the rail network, for visualisation purposes.
+        You can optionally pass a marker size (in pixels) for the plot to use.
+
+        The method will produce a matplotlib figure showing the locations of the stations in the network, and
+        attempt to use matplotlib.pyplot.show to display the figure.
+
+        This function will not execute successfully until you have created the regions() function.
+        You are NOT required to write tests nor documentation for this function.
+        """
+        fig, ax = plt.subplots(figsize=(5, 10))
+        ax.set_xlabel("Longitude (degrees)")
+        ax.set_ylabel("Latitude (degrees)")
+        ax.set_title("Railway Network")
+
+        COLOURS = ["b", "r", "g", "c", "m", "y", "k"]
+        MARKERS = [".", "o", "x", "*", "+"]
+
+        for i, r in enumerate(self.regions):
+            lats = [s.lat for s in self.stations.values() if s.region == r]
+            lons = [s.lon for s in self.stations.values() if s.region == r]
+
+            colour = COLOURS[i % len(COLOURS)]
+            marker = MARKERS[i % len(MARKERS)]
+            ax.scatter(lons, lats, s=marker_size, c=colour, marker=marker, label=r)
+
+        ax.legend()
+        plt.tight_layout()
+        plt.show()
+        return
+
+    def plot_journey(self, start: str, dest: str) -> None:
+        """
+        Plot the journey between the start and end stations, on top of the rail network map.
+        The start and dest inputs should the strings corresponding to the CRS codes of the
+        starting and destination stations, respectively.
+
+        The method will overlay the route that your journey_planner method has found on the
+        locations of the stations in your network, and draw lines to indicate the route.
+
+        This function will not successfully execute until you have written the journey_planner method.
+        You are NOT required to write tests nor documentation for this function.
+        """
+        # Plot railway network in the background
+        network_lats = [s.lat for s in self.stations.values()]
+        network_lons = [s.lon for s in self.stations.values()]
+
+        fig, ax = plt.subplots(figsize=(5, 10))
+        ax.scatter(network_lons, network_lats, s=1, c="blue", marker="x")
+        ax.set_xlabel("Longitude (degrees)")
+        ax.set_ylabel("Latitude (degrees)")
+
+        # Compute the journey
+        journey = self.journey_planner(start, dest)
+        plot_title = f"Journey from {journey[0].name} to {journey[-1].name}"
+        ax.set_title(f"Journey from {journey[0].name} to {journey[-1].name}")
+
+        # Draw over the network with the journey
+        journey_lats = [s.lat for s in journey]
+        journey_lons = [s.lon for s in journey]
+        ax.plot(journey_lons, journey_lats, "ro-", markersize=2)
+
+        plt.show()
+        return
